2d1ac2ff854aed837dd81cd4c7bec7f3476656ed
Александр <fedoseevale@gmail.com>
2022-10-16 11:36:59 +0400

    Ex25 done

--- Execise25/Program.cs
+++ Execise25/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int NumberAPowB (int numA, int numB)
+{
+   int result = 1;   
+   for (int i = 1; i <= numB; i++)
+   {
+        result = result*numA;
+   }
+   return result;
+}
+
+
+int numberAPowB = NumberAPowB(numberA, numberB);
+
+if (numberB >= 1)
+{
+    Console.WriteLine ($"Результатом возведения числа A в натуральную степень B является число {numberAPowB}");
+}
+else Console.WriteLine ("Число B не является натуральным");
+
+
+
+
+
+
+
