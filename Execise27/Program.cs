8384923459740f57cfa4ae849946fca379621d09
Александр <fedoseevale@gmail.com>
2022-10-17 11:18:28 +0400

    Ex27 done

--- Execise27/Program.cs
+++ Execise27/Program.cs
@@ -2,3 +2,22 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits (int num)
+{
+   int result = num % 10;   
+   for (int i = 1; i <= num; i++)
+   {
+        int step1 = num / 10;
+        num = step1;
+        int step2 = num % 10; 
+        result = result + step2;
+   }
+   return result;
+}
+
+int sumDigits = SumDigits (number);
+Console.WriteLine($"Сумма цифр в данном числе: {sumDigits}");
